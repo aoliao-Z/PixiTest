dd65a07140dee6a560634d977119c740cef0404d
野漫横江 <1399226280@qq.com>
2022-10-08 18:01:50 +0800

    feat: 定义矩形及圆的基本类型

--- src/components/View/components/types.ts
+++ src/components/View/components/types.ts
@@ -1,4 +1,4 @@
-import { Graphics } from "pixi.js"
+import { DisplayObject, Graphics } from "pixi.js"
 
 export type ComponentType = "button"
 export type Pos = {
@@ -11,7 +11,13 @@ export type Size = {
     height: number
 }
 
+/** 基础矩形 */
 export type IBaseRect = Pos & Size
+
+/** 基础圆 */
+export type IBaseCircle = Pos & {
+    r: number
+}
 
 export type ComponentStyle = {}
 
@@ -19,7 +25,9 @@ export interface IComponent {
     type: ComponentType
     box: IBaseRect
     style: ComponentStyle
-    build(): Graphics
+    build(): DisplayObject // pixi显示层基类
 }
 
 export interface IButton extends IComponent {}
+
+// TODO: 组件设置泛型? 然后工厂函数生成指定组件?
