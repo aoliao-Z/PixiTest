360b1e4550b10b0323393f1ccde2059f7ef98cba
野漫横江 <1399226280@qq.com>
2022-10-09 16:07:00 +0800

    feat: 添加api`copyConfig`用于填充可选项

--- src/components/View/components/style/utils.ts
+++ src/components/View/components/style/utils.ts
@@ -19,6 +19,28 @@ export const parseBorder = (cssBorder: string | BorderStyle): BorderStyle => {
     throw `传递的border值有误, border: ${widths}`
 }
 
+/** 复制配置项 */
+export const copyConfig = (source: any, target: any): any => {
+    const isBaseType = (value: any) => {
+        const type = typeof value
+        if (type === "function" || type === "symbol") throw "配置文件不应该有函数类型或者symbol类型"
+        if (type === "object") return false
+        return true
+    }
+    const isBase = isBaseType(target)
+    if (isBase) return source ?? target // source存在则读取source 否则读取target
+    const obj = {} as any // 如果是对象类型, 则需要深度克隆
+    for (const key in target) {
+        if (!source || !source[key]) {
+            // 如果不存在配置项, 且该字段为基本类型, 则读取target配置
+            if (isBase) obj[key] = target[key]
+            // 如果不是基本类型, 则递归读取
+            else obj[key] = copyConfig(undefined, target[key])
+        }
+    }
+    return obj
+}
+
 if (import.meta.vitest) {
     const { it, expect } = import.meta.vitest
     it("all", () => {
@@ -46,6 +68,45 @@ if (import.meta.vitest) {
         const target = [1, 2, 3, 4]
         const source = parseBorder(border)
         expect(source).toEqual(target)
+    })
+
+    it("copyConfig", () => {
+        const base = {}
+        const config = {
+            a: 1,
+            b: 1,
+            c: {
+                d: {
+                    v: 1,
+                },
+                f: {
+                    p: {
+                        d: 2,
+                        e: true,
+                        r: 1.0,
+                    },
+                    o: "string",
+                },
+            },
+        }
+        const target = {
+            a: 1,
+            b: 1,
+            c: {
+                d: {
+                    v: 1,
+                },
+                f: {
+                    p: {
+                        d: 2,
+                        e: true,
+                        r: 1.0,
+                    },
+                    o: "string",
+                },
+            },
+        }
+        expect(copyConfig(base, config)).toEqual(target)
     })
 }
 
